f5bb0a2c82ec4b41a4146049500e8f02b7e11f0b
mohammadlotfi <mohammad.lotfi7913@gmail.com>
2025-07-21 23:54:04 +0330

    implement show confirm dialog when deleting a community member

--- frontend-cakedaylog/app/community-members/[id]/CommunityMembersList.client.tsx
+++ frontend-cakedaylog/app/community-members/[id]/CommunityMembersList.client.tsx
@@ -9,18 +9,30 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import React from "react";
+import React, { useRef, useState } from "react";
 import {
   useCommunityMembers,
   useDeleteCommunityMember,
 } from "../../../lib/hooks/communityMembersHooks";
 import { Member } from "../../../lib/types/member";
+import {
+  Dialog,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 interface Props {
   communitId: string;
 }
 
 const CommunityMembersList = ({ communitId }: Props) => {
+  const closeRef = useRef<HTMLButtonElement>(null);
   const { data: membersList } = useCommunityMembers(communitId);
   const { mutate: deleteCommunityMember } = useDeleteCommunityMember();
 
@@ -42,18 +54,43 @@ const CommunityMembersList = ({ communitId }: Props) => {
               <TableCell>{member.firstname}</TableCell>
               <TableCell>{member.lastname}</TableCell>
               <TableCell>{member.birthdate}</TableCell>
+
               <TableCell>
-                <button
-                  onClick={async () => {
-                    deleteCommunityMember({
-                      communityId: communitId,
-                      memberId: member._id,
-                    });
-                  }}
-                  className="bg-error text-white rounded-md p-1.5 cursor-pointer"
-                >
-                  Action
-                </button>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button variant="outline">Delete</Button>
+                  </DialogTrigger>
+
+                  <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                      <DialogTitle>Delete Community Member</DialogTitle>
+                      <DialogDescription>
+                        Are you sure you want to delete {member.firstname}{" "}
+                        {member.lastname}?
+                      </DialogDescription>
+                    </DialogHeader>
+
+                    <DialogFooter>
+                      <DialogClose asChild>
+                        <Button variant={"outline"}>Cancel</Button>
+                      </DialogClose>
+                      <Button
+                        onClick={async () => {
+                          await deleteCommunityMember({
+                            communityId: communitId,
+                            memberId: member._id,
+                          });
+                          closeRef.current?.click();
+                        }}
+                      >
+                        Yes!
+                      </Button>
+                      <DialogClose asChild>
+                        <button ref={closeRef} className="hidden" />
+                      </DialogClose>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
               </TableCell>
             </TableRow>
           ))}
